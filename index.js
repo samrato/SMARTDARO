0a1edef03615e6a6bd817c209f3aabe3cffdd137
Willington Juma <willingtonejumamconyango@gmail.com>
2025-03-01 00:20:56 +0300

    .

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 const allowedOrigins = [
-  "http://localhost:5173", // Add your frontend URL here ni god manze
+  "http://localhost:5173", // Add your frontend URL here ni god manze....
   "https://yourfrontenddomain.com" // If you have a live domain, add it here
 ];
 
